8659d5ba73106994e71f90ea96debb2534706fb9
NailSunny <gaifullinnail7@gmail.com>
2025-03-03 12:53:14 +0300

    fff

---
+++ lib/chats.dart
@@ -0,0 +1,160 @@
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatefulWidget {
+  const ChatsPage({super.key});
+
+  @override
+  State<ChatsPage> createState() => _ChatsPageState();
+}
+
+class _ChatsPageState extends State<ChatsPage> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, dynamic>> chats = [
+    {"name": "Наиль", "lastMessage": "Привет!", "time": "12:30", "read": true},
+    {
+      "name": "Латифа",
+      "lastMessage": "Привет!",
+      "time": "12:30",
+      "read": false
+    },
+    {"name": "Маша", "lastMessage": "Привет!", "time": "12:30", "read": true},
+    {
+      "name": "Юлиана",
+      "lastMessage": "Привет!",
+      "time": "12:30",
+      "read": false
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 16,
+              child: Icon(Icons.person),
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.02,
+            ),
+            Text("Chats"),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.edit),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.93,
+              child: TextField(
+                cursorColor: Colors.black,
+                decoration: InputDecoration(
+                  labelText: 'Search',
+                  labelStyle: TextStyle(color: Colors.grey),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.grey,
+                  ),
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.015),
+            Expanded(
+              child: ListView.builder(
+                itemCount: chats.length,
+                itemBuilder: (context, index) {
+                  final chat = chats[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      radius: 20,
+                      child: Icon(Icons.person),
+                    ),
+                    title: Text(chats[index]["name"]!),
+                    subtitle: Text(
+                      chat["lastMessage"],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        color: chat["read"] ? Colors.grey : Colors.black,
+                        fontWeight:
+                            chat["read"] ? FontWeight.normal : FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          chat["time"],
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        chat["read"]
+                            ? Icon(
+                                Icons.done_all,
+                                color: Colors.blue,
+                                size: 18,
+                              )
+                            : CircleAvatar(
+                                backgroundColor: Colors.blue,
+                                radius: 8,
+                              ),
+                      ],
+                    ),
+                    onTap: () {},
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue,
+        unselectedItemColor: Colors.grey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.contacts),
+            label: 'Contacts',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+          if (index == 0) {
+            //Navigator.popAndPushNamed(context, '/chats');
+          } else if (index == 1) {
+            Navigator.popAndPushNamed(context, '/contacts');
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:mess_scuf/auth.dart';
+import 'package:mess_scuf/chats.dart';
+import 'package:mess_scuf/contacts.dart';
+import 'package:mess_scuf/recovery.dart';
+import 'package:mess_scuf/reg.dart';
+
+void main() {
+  runApp(const AppTheme());
+}
+
+class AppTheme extends StatelessWidget {
+  const AppTheme({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.white,
+            centerTitle: true,
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          outlinedButtonTheme: OutlinedButtonThemeData(
+            style: ButtonStyle(
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      side: BorderSide(color: Colors.blue)),
+                ),
+                backgroundColor: WidgetStatePropertyAll(Colors.transparent),
+                foregroundColor: WidgetStatePropertyAll(Colors.black)),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ButtonStyle(
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                backgroundColor: WidgetStatePropertyAll(Colors.lightBlue),
+                foregroundColor: WidgetStatePropertyAll(Colors.white)),
+          )),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => ChatsPage(),
+        '/reg': (context) => RegPage(),
+        '/rec': (context) => RecoveryPage(),
+        '/chats': (context) => ChatsPage(),
+        '/contacts': (context) => ContactsPage(),
+      },
+    );
+  }
+}
